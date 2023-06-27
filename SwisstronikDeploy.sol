3e7fdf989363b02ff128a42917c8f5f9a274fd63
itsLegendSky <137892093+itsLegendSky@users.noreply.github.com>
2023-06-27 18:27:27 +0700

    SwisstronikDeploy.sol
    adding .sol file

---
+++ SwisstronikDeploy.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Register {
+    string public github;
+    address public owner;
+    
+    struct Referral {
+        address referralAddress;
+        string referralString;
+    }
+    
+    Referral[] public referrals;
+    
+    constructor() {
+        github = "you_user"; //example : itsLegendSky
+        owner = “0x_address”; //example : 0x69420xxxx
+    }
+    
+    function addReferral(address _referralAddress, string memory _referralString) external {
+        require(msg.sender == owner, "Only the owner can add referrals.");
+        referrals.push(Referral(_referralAddress, _referralString));
+    }
+    
+    function totalReferrals() public view returns (uint256) {
+        return referrals.length;
+    }
+}
+
+//My Discord : sexxos [LegendSky]
